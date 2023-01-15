d120f04dbbc5c84f0ea9a502b17f21e5962cacfa
yuken <yuken0906dev@gmail.com>
2023-01-15 22:53:45 +0900

    refactor book hitsory schema

--- server/services/bookHistory.ts
+++ server/services/bookHistory.ts
@@ -6,24 +6,25 @@ import { UserModel } from 'server/models/user';
 
 export const BookHistoryService: IBookHistoryService = {
   async postBookHistory(userId: string, isbn: number): Promise<IBookHistory> {
-    const bookInDb = await BookModel.findOne({ isbn });
-    // すでに登録されているならエラーを返す
-    const bookHistoryInDb = await BookHistoryModel.findOne({
-      user_id: userId,
-      book_id: bookInDb?._id,
-    });
-    if (bookHistoryInDb) {
-      throw Error('already book history registered');
+    try {
+      const bookInDb = await BookModel.findOne({ isbn });
+      // すでに登録されているならエラーを返す
+      const bookHistoryInDb = await BookHistoryModel.getByUserAndBook(userId, bookInDb?._id);
+      if (bookHistoryInDb) {
+        throw Error('already book history registered');
+      }
+      const user = await UserModel.getById(userId);
+
+      const bookHistoryObj = {
+        user: user,
+        book: bookInDb,
+      };
+
+      const bookHistory = new BookHistoryModel(bookHistoryObj);
+
+      return await bookHistory.save();
+    } catch {
+      throw Error('can not save book history');
     }
-    const user = await UserModel.getById(userId);
-
-    const bookHistoryObj = {
-      user_id: user,
-      book_id: bookInDb,
-    };
-
-    const bookHistory = new BookHistoryModel(bookHistoryObj);
-
-    return await bookHistory.save();
   },
 };
