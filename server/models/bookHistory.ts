0ff5f5f6530485cb3528ee283be4dc7ee863946f
yuken <yuken0906dev@gmail.com>
2023-01-21 14:25:17 +0900

    add timestamp

--- server/models/bookHistory.ts
+++ server/models/bookHistory.ts
@@ -2,10 +2,13 @@ import { IBookHistoryDocument, IBookHistoryModel } from 'interface/models/bookHi
 import mongoose, { Schema } from 'mongoose';
 import { getOrCreateModel } from 'server/utils/mongoose';
 
-const BookHistorySchema = new mongoose.Schema<IBookHistoryDocument>({
-  book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-});
+const BookHistorySchema = new mongoose.Schema<IBookHistoryDocument>(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true },
+);
 
 BookHistorySchema.statics.getByUserAndBook = async function (userId: string, bookId: string) {
   return this.findOne({ user: userId, book: bookId }).exec();
--- server/models/review.ts
+++ server/models/review.ts
@@ -2,11 +2,14 @@ import { IReviewDocument, IReviewModel } from 'interface/models/review';
 import mongoose, { Schema } from 'mongoose';
 import { getOrCreateModel } from 'server/utils/mongoose';
 
-const ReviewSchema = new mongoose.Schema<IReviewDocument>({
-  book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
-  reviewer: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
-});
+const ReviewSchema = new mongoose.Schema<IReviewDocument>(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+    reviewer: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
 ReviewSchema.statics.getByUserAndBook = async function (userId: string, bookId: string) {
   return this.findOne({ book: bookId, reviewer: userId }).exec();
