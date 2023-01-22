2f4ea04f08c112b51e39a64fbea84c4e2b5c6037
yuken <yuken0906dev@gmail.com>
2023-01-22 20:00:15 +0900

    implement official twitter footer

--- client/components/organisms/Footer.tsx
+++ client/components/organisms/Footer.tsx
@@ -1,13 +1,16 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Flex, Image, Link } from '@chakra-ui/react';
 
 export const Footer = (): JSX.Element => {
   return (
     <Box>
       <Flex p={4} bg='white' justifyContent='center'>
-        <Link mr={4} href='https://yukendev.github.io/our-hontai-terms' isExternal>
+        <Link mx={2} href='https://twitter.com/our_hontai' isExternal>
+          <Image w={6} src='/twitter.png' alt='twitter' />
+        </Link>
+        <Link mx={2} href='https://yukendev.github.io/our-hontai-terms' isExternal>
           利用規約
         </Link>
-        <Link ml={4} href='https://forms.gle/k32FqsFNWDLn2sRP9' isExternal>
+        <Link mx={2} href='https://forms.gle/k32FqsFNWDLn2sRP9' isExternal>
           お問い合せ
         </Link>
       </Flex>
