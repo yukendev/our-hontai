67860a172192451b90cc3cc614faced5c26c4791
yuken <yuken0906dev@gmail.com>
2023-01-22 22:56:57 +0900

    refactor mongo setup

--- server/utils/mongoose.ts
+++ server/utils/mongoose.ts
@@ -7,3 +7,18 @@ export const getOrCreateModel = <Document, Model>(modelName: string, schema: Sch
   }
   return mongoose.model<Document, Model>(modelName, schema);
 };
+
+export const setupMongo = async () => {
+  const { MONGODB_URI: uri, MONGODB_DB: dbName } = process.env;
+
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return;
+  }
+
+  if (!uri) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  }
+
+  await mongoose.connect(uri, { dbName });
+};
