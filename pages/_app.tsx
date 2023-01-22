c1807b4a849c63e0b1f7cbca6d192f939d48aa43
yuken <yuken0906dev@gmail.com>
2023-01-22 17:36:06 +0900

    default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { colors } from 'client/styles/colors';
 import '../client/styles/global.scss';
+import { DefaultSeo } from 'next-seo';
 
 const theme = extendTheme({
   styles: {
@@ -17,10 +18,38 @@ const theme = extendTheme({
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <>
+      <DefaultSeo
+        defaultTitle='みんなで本屋大賞'
+        description='本屋大賞ファンによる、本屋大賞ファンのためのサービスです。最新の本屋大賞ノミネート作品から、過去のノミネート作品までまとめています。Googleアカウントと連携をすることで感想を投稿したり、自分だけの読書記録を作ることもできます'
+        openGraph={{
+          type: 'website',
+          title: 'みんなで本屋大賞',
+          description:
+            '本屋大賞ファンによる、本屋大賞ファンのためのサービスです。最新の本屋大賞ノミネート作品から、過去のノミネート作品までまとめています。Googleアカウントと連携をすることで感想を投稿したり、自分だけの読書記録を作ることもできます',
+          site_name: 'みんなで本屋大賞',
+          url: 'https://our-hontai.com',
+          images: [
+            {
+              url: 'https://our-hontai.com/ogp.png',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@our_hontai',
+          site: '@our_hontai',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </>
   );
 }
